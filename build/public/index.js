b9ef171615f4dd65aa424508ff2bdf68493d9520
Dragomir Ceban <svedava@mail.ru>
2018-01-10 15:59:55 +0000

    Update index.js

--- build/public/index.js
+++ build/public/index.js
@@ -1,16 +1,5 @@
 "use strict";
 
-var sayHello = function sayHello() {
-  console.log("say hello");
-};
-
-sayHello();
-
-var sayhi = function sayhi() {
-  console.log("say hi to me now!");
-};
-
-sayhi();
 
 var userButtons = document.getElementsByClassName("user__buttons")[0];
 var loginButton = document.getElementsByClassName("home__buttons__login")[0];
