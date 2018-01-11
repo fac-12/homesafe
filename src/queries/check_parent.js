63e7aee4d7c1d74a876e52fe218e405f2fae23c2
parkhabit <parkhabit@gmail.com>
2018-01-11 10:24:10 +0000

    #11 added check_parent query

---
+++ src/queries/check_parent.js
@@ -0,0 +1,8 @@
+const db_connections = require('../database/db_connections.js')
+
+const check_parent = (email) => {
+  db_connections.query(
+    `SELECT CASE WHEN EXISTS(SELECT email FROM parents WHERE email = $1) THEN CAST (true AS BOOLEAN) ELSE CAST (false AS BOOLEAN) END`, [email], (err, res)
+  };
+
+module.exports = check_parent;
