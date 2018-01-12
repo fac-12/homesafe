10a741b9f0a46a653674e38d160785a23cd98dcf
parkhabit <parkhabit@gmail.com>
2018-01-12 09:43:41 +0000

    updated queries

--- src/queries/add_pickup.js
+++ src/queries/add_pickup.js
@@ -1,5 +1,5 @@
 const db_connections = require('../database/db_connections');
 
-const add_pickup = (parent_id, child_id, pickup_date, keyword) => db_connections.query('INSERT INTO scheduled_pickups (parent_id, child_id, pickup_date, keyword) VALUES ($1, $2, $3, $4)', [parent_id, child_id, pickup_date, keyword])
+const add_pickup = (pickup_info) => db_connections.query('INSERT INTO scheduled_pickups (parent_id, child_id, pickup_date, keyword) VALUES ($1, $2, $3, $4)', [pickup_info.parent_id, pickup_info.child_id, pickup_info.pickup_date, pickup_info.keyword])
 
 module.exports = add_pickup;
