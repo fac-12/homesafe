f15da4afd3e7daffdb47d27fcf70591f8ef54384
Dragomir Ceban <svedava@mail.ru>
2018-01-12 16:04:46 +0000

    Update add_child.js

--- src/routes/add_child.js
+++ src/routes/add_child.js
@@ -5,7 +5,6 @@ const add_child = require('../queries/add_child')
 exports.post = (req, res) => {
 if (req.session.loggedin === true) {
   const child_details = req.body;
-  console.log(child_details);
   child_details.school_id = 2;
   child_details.parent_id = req.session.parent_id;
   add_child(child_details).then(()=>{
