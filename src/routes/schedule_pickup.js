8ee97c8022e6bd1afa5f18b108989abfe2eed4d7
Gbaja <gbajaf@yahoo.co.uk>
2018-01-16 13:19:38 +0000

    Added header_banner partial and started fixing schedule pick up bug

--- src/routes/schedule_pickup.js
+++ src/routes/schedule_pickup.js
@@ -9,25 +9,27 @@ const {
 
 
 exports.post = (req, res) => {
-  const schedule_details = req.body;
-  parent_child_da_id(req.session.parent_id, schedule_details.child_name, schedule_details.da_name).then((queryRes) => {
+  parent_child_da_id(req.session.parent_id, req.body.child_name, req.body.da_name).then((queryRes) => {
       return queryRes;
     }).then(queryRes => {
       const schedule_info = {};
       schedule_info.parent_id = req.session.parent_id,
         schedule_info.child_id = queryRes[0].child_id,
         schedule_info.da_id = queryRes[0].da_id,
-        schedule_info.pickup_date = schedule_details.pickup_date,
-        schedule_info.keyword = schedule_details.keyword
+        schedule_info.pickup_date = req.body.pickup_date,
+        schedule_info.keyword = req.body.keyword
       return add_pickup(schedule_info);
     }).then(queryRes => {
       return get_parent_da_email(queryRes[0].designated_adult_id, queryRes[0].parent_id);
     }).then((emails) => {
       parent_pickup_confirmation_email(emails[0].email);
-      da_pickup_confirmation_email(emails[0].da_email, schedule_details.keyword);
+      da_pickup_confirmation_email(emails[0].da_email, req.body.keyword);
     return;
     })
-    .then(() => res.render('pickup_confirmation'))
+    .then(() => {
+      req.flash('success', `Your pickup schedule has been added. Your keyword is ${req.body.keyword}`)
+      res.redirect('parent_profile')
+    })
     .catch((err) => {
       res.status(500).render('error', {
         layout: 'error',
