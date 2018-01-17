1e8bc42f3a4fb7fed623db859d33254c676c0a6b
dragomir <svedava@mail.ru>
2018-01-17 14:57:04 +0000

    started schoolClientSideValidation realtes #56

--- build/public/index.js
+++ build/public/index.js
@@ -7,7 +7,7 @@ sayHello();
 
 var parent_password = document.getElementById('parent-registration-password');
 var parent_registration_submit = document.getElementById('parent-registration-submit');
-var parent_password_input = document.getElementById('parent-registration-password');
+// const parent_password_input = document.getElementById('parent-registration-password')
 var parent_strong_password_err = document.getElementById('strong_password_err');
 var parent_registration_confirm_password = document.getElementById('parent-registration-confirm-password');
 var confirm_password_err = document.getElementById('confirm_password_err');
@@ -26,8 +26,9 @@ var strongPassword = function strongPassword(password, display_error, button) {
 };
 
 var emailValid = function emailValid(email, display_error, button) {
-  var regex = new RegExp('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-  !regex.test(email.value) ? display_error.textContent = "Please enter a valid email." : button.disabled = false;
+  // const regex = new RegExp ('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  var regex = new RegExp('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
+  !regex.test(email.value) ? display_error.textContent = "Please enter a valid email." : (button.disabled = false, display_error.textContent = "");
 };
 
 var childYear = function childYear(child_year, button) {
@@ -39,18 +40,33 @@ var checkPhone = function checkPhone(phone, display_error, button) {
   return !regex.test(phone.value) ? display_error.textContent = "Please enter a valid UK phone number" : (button.disabled = false, display_error.textContent = "");
 };
 
-// parent_password_input.addEventListener("keyup", ()=>{
-//   strongPassword(parent_password, parent_strong_password_err, parent_registration_submit)
-// });
-//
-// parent_registration_confirm_password.addEventListener("keyup", ()=>{
-//   confirmPassword(parent_password_input, parent_registration_confirm_password, confirm_password_err, parent_registration_submit)
-// });
-//
-// parent_registration_email.addEventListener("keyup", ()=>{
-//   emailValid(parent_registration_email, parent_email_err, parent_registration_submit)
-// })
-//
-// parent_registration_phone.addEventListener('keyup', ()=>{
-//   checkPhone(parent_registration_phone, parent_phone_err, parent_registration_submit)
-// })
+parent_password.addEventListener("keyup", function () {
+  strongPassword(parent_password, parent_strong_password_err, parent_registration_submit);
+});
+
+parent_registration_confirm_password.addEventListener("keyup", function () {
+  confirmPassword(parent_password, parent_registration_confirm_password, confirm_password_err, parent_registration_submit);
+});
+
+parent_registration_email.addEventListener("keyup", function () {
+  emailValid(parent_registration_email, parent_email_err, parent_registration_submit);
+});
+
+parent_registration_phone.addEventListener('keyup', function () {
+  checkPhone(parent_registration_phone, parent_phone_err, parent_registration_submit);
+});
+
+var school_password = document.getElementById('school-registration-password');
+var school_registration_submit = document.getElementById('school-registration-submit');
+var school_registration_email = document.getElementById('school-registration-email');
+var school_registration_confirm_password = document.getElementById('school-registration-confirm-password');
+school_password.addEventListener("keyup", function () {
+  strongPassword(school_password, strong_password_err, school_registration_submit);
+});
+school_registration_confirm_password.addEventListener("keyup", function () {
+  confirmPassword(school_password, school_registration_confirm_password, confirm_password_err, school_registration_submit);
+});
+school_registration_email.addEventListener("keyup", function () {
+  emailValid(school_registration_email, email_err, school_registration_submit);
+});
+var email_err = document.getElementById('email_err');
--- public/index.js
+++ public/index.js
@@ -5,7 +5,7 @@ sayHello();
 
 const parent_password = document.getElementById('parent-registration-password');
 const parent_registration_submit = document.getElementById('parent-registration-submit');
-const parent_password_input = document.getElementById('parent-registration-password')
+// const parent_password_input = document.getElementById('parent-registration-password')
 const parent_strong_password_err = document.getElementById('strong_password_err');
 const parent_registration_confirm_password = document.getElementById('parent-registration-confirm-password');
 const confirm_password_err = document.getElementById('confirm_password_err')
@@ -28,8 +28,10 @@ const strongPassword = (password, display_error, button) => {
 }
 
 const emailValid = (email, display_error, button) => {
-  const regex = new RegExp ('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-  !regex.test(email.value) ? display_error.textContent = "Please enter a valid email." : button.disabled = false;
+    // const regex = new RegExp ('^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+  const regex = new RegExp('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$');
+  !regex.test(email.value) ? display_error.textContent = "Please enter a valid email." : (button.disabled = false,
+    display_error.textContent = "");
 }
 
 const childYear = (child_year, button) => {
@@ -43,18 +45,33 @@ const checkPhone = (phone, display_error, button) => {
     display_error.textContent = "");
 }
 
-// parent_password_input.addEventListener("keyup", ()=>{
-//   strongPassword(parent_password, parent_strong_password_err, parent_registration_submit)
-// });
-//
-// parent_registration_confirm_password.addEventListener("keyup", ()=>{
-//   confirmPassword(parent_password_input, parent_registration_confirm_password, confirm_password_err, parent_registration_submit)
-// });
-//
-// parent_registration_email.addEventListener("keyup", ()=>{
-//   emailValid(parent_registration_email, parent_email_err, parent_registration_submit)
-// })
-//
-// parent_registration_phone.addEventListener('keyup', ()=>{
-//   checkPhone(parent_registration_phone, parent_phone_err, parent_registration_submit)
-// })
+parent_password.addEventListener("keyup", ()=>{
+  strongPassword(parent_password, parent_strong_password_err, parent_registration_submit)
+});
+
+parent_registration_confirm_password.addEventListener("keyup", ()=>{
+  confirmPassword(parent_password, parent_registration_confirm_password, confirm_password_err, parent_registration_submit)
+});
+
+parent_registration_email.addEventListener("keyup", ()=>{
+  emailValid(parent_registration_email, parent_email_err, parent_registration_submit)
+})
+
+parent_registration_phone.addEventListener('keyup', ()=>{
+  checkPhone(parent_registration_phone, parent_phone_err, parent_registration_submit)
+})
+
+const school_password = document.getElementById('school-registration-password');
+const school_registration_submit = document.getElementById('school-registration-submit');
+const school_registration_email = document.getElementById('school-registration-email');
+const school_registration_confirm_password = document.getElementById('school-registration-confirm-password');
+school_password.addEventListener("keyup", ()=>{
+strongPassword(school_password,strong_password_err, school_registration_submit)
+});
+school_registration_confirm_password.addEventListener("keyup", ()=>{
+confirmPassword(school_password, school_registration_confirm_password, confirm_password_err, school_registration_submit)
+});
+school_registration_email.addEventListener("keyup", ()=>{
+emailValid(school_registration_email, email_err, school_registration_submit)
+});
+const email_err = document.getElementById('email_err');
