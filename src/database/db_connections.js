5b1aa22b490391d7ea6d8b5bcb6dadad4ca269d4
sophielevens <sophielevens1@gmail.com>
2018-01-17 16:17:20 +0000

    updated process.env again

--- src/database/db_connections.js
+++ src/database/db_connections.js
@@ -6,7 +6,7 @@ let options = {};
 let DATABASE_URL = process.env.DATABASE_URL;
 if (process.env.NODE_ENV === "test") {
   DATABASE_URL = process.env.TEST_DB_URL;
-} else if (process.env.TRAVIS) {
+} else if (process.env.TRAVIS === true) {
   options = {
     database: 'travis_ci_test'
   }
@@ -28,5 +28,5 @@ const [username, password] = params.auth.split(':');
 options.ssl = options.host !== 'localhost';
 }
 
-console.log(options);
+console.log(options, 'MMMM<<<<<<');
 module.exports = pgp(options);
