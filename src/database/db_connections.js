e37dbae3654fcd717e4d5bc1ab4ca4f58eba2d2a
dragomir <svedava@mail.ru>
2018-01-18 15:50:40 +0000

    reverted to the old db_connections.js

--- src/database/db_connections.js
+++ src/database/db_connections.js
@@ -1,31 +1,67 @@
+// const pgp = require('pg-promise')();
+// const url = require('url');
+// require('env2')('./config.env');
+// let options = {};
+//
+// let DATABASE_URL = process.env.DATABASE_URL;
+// if (process.env.NODE_ENV === "test") {
+//   DATABASE_URL = process.env.TEST_DB_URL;
+// } else if (process.env.TRAVIS === true) {
+//   options = {
+//     database: 'travis_ci_test'
+//   }
+// } else if (!DATABASE_URL) {
+//    throw new Error("Enviroment variable DATABASE_URL must be set");
+// } else {
+// const params = url.parse(DATABASE_URL);
+// const [username, password] = params.auth.split(':');
+//
+//  options = {
+//     host: params.hostname,
+//     port: params.port,
+//     database: params.pathname.split('/')[1],
+//     max: process.env.DB_MAX_CONNECTIONS || 2,
+//     user: username,
+//     password: password
+// };
+//
+// options.ssl = options.host !== 'localhost';
+// }
+//
+// module.exports = pgp(options);
+
 const pgp = require('pg-promise')();
 const url = require('url');
 require('env2')('./config.env');
 let options = {};
 
 let DATABASE_URL = process.env.DATABASE_URL;
-if (process.env.NODE_ENV === "test") {
-  DATABASE_URL = process.env.TEST_DB_URL;
-} else if (process.env.TRAVIS === true) {
+if (process.env.TRAVIS === 'true') {
+
   options = {
     database: 'travis_ci_test'
-  }
-} else if (!DATABASE_URL) {
-   throw new Error("Enviroment variable DATABASE_URL must be set");
-} else {
-const params = url.parse(DATABASE_URL);
-const [username, password] = params.auth.split(':');
-
- options = {
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    max: process.env.DB_MAX_CONNECTIONS || 2,
-    user: username,
-    password: password
-};
-
-options.ssl = options.host !== 'localhost';
 }
 
-module.exports = pgp(options);
+  } else {
+    if (!DATABASE_URL && process.env.TRAVIS != 'true') {
+      throw new Error("Enviroment variable DATABASE_URL must be set");
+    } else if (process.env.NODE_ENV === "test") {
+        DATABASE_URL = process.env.TEST_DB_URL;
+      }
+      const params = url.parse(DATABASE_URL);
+      const [username, password] = params.auth.split(':');
+
+      options = {
+        host: params.hostname,
+        port: params.port,
+        database: params.pathname.split('/')[1],
+        max: process.env.DB_MAX_CONNECTIONS || 2,
+        user: username,
+        password: password
+      };
+
+      options.ssl = options.host !== 'localhost';
+    }
+
+
+  module.exports = pgp(options);
